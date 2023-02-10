6f19017e729171b0d01927a0fa95fdc0af97b7ab
JacopoPassariello <j.passariello@studenti.unisa.it>
2023-02-10 16:22:39 +0100

    aggiunto distinct alle query, sistemato group by.

--- SQL/query_database.sql
+++ SQL/query_database.sql
@@ -4,15 +4,13 @@ select v.targa, v.scadenza_assicurazione, v.guidatore as cf_guidatore
 from Veicolo v
 where (YEAR(v.scadenza_assicurazione) > YEAR(CURDATE()) or YEAR(v.scadenza_assicurazione) = YEAR(curdate())) and v.guidatore is not null;
 
-select s.civico, s.via, s.cap, s.citta, s.tipo
+select distinct s.civico, s.via, s.cap, s.citta, s.tipo
 from Sede s, Veicolo v
-where v.sede = s.id
-group by s.id;
+where v.sede = s.id;
 
-select s.civico, s.via, s.cap, s.citta, s.tipo
+select distinct s.civico, s.via, s.cap, s.citta, s.tipo
 from Sede s, Macchinario m
-where m.sede = s.id
-group by s.id;
+where m.sede = s.id;
 
 select SUM(d.paga_oraria * d.ore_lavoro + d.bonus) as totale_stipendi
 from Dipendente d;
